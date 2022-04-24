ff3a18581d73941fe520120fe2b239cf738b2b29
André Dal Bosco <andredbsc@gmail.com>
2022-04-24 08:13:40 +0200

    update contract

--- contracts/LeibnizVault.sol
+++ contracts/LeibnizVault.sol
@@ -21,4 +21,68 @@ contract Vault is ERC4626 {
         underlyingToken = _underlyingToken;
         totalSupply = type(uint256).max;
     }
+    
+   function afterDeposit(uint256, uint256) internal override {}
+
+    function beforeWithdraw(uint256 assets, uint256) internal override {
+        retrieveUnderlying(assets);
+    }
+
+    function retrieveUnderlying(uint256 underlyingAmount) internal {
+        uint256 float = totalFloat();
+
+        if (underlyingAmount > float) {
+            uint256 floatMissingForTarget = (totalAssets() - underlyingAmount).mulWadDown(targetFloatPercent);
+            uint256 floatMissingForWithdrawal = underlyingAmount - float;
+            pullFromWithdrawalStack(floatMissingForWithdrawal + floatMissingForTarget);
+        }
+    }
+
+    function balanceOfUnderlying(address user) external view returns (uint256) {
+        return balanceOf[user].mulDivDown(exchangeRate(), BASE_UNIT);
+    }
+
+    function exchangeRate() public view returns (uint256) {
+        uint256 rvTokenSupply = totalSupply;
+        if (rvTokenSupply == 0) return BASE_UNIT;
+        return totalAssets().mulDivDown(BASE_UNIT, rvTokenSupply);
+    }
+
+    function totalAssets() public view override returns (uint256 totalUnderlyingHeld) {
+        unchecked {
+            totalUnderlyingHeld = totalStrategyHoldings - lockedProfit();
+        }
+        totalUnderlyingHeld += totalFloat();
+    }
+
+    function lockedProfit() public view returns (uint256) {
+        uint256 previousHarvest = lastHarvest;
+        uint256 harvestInterval = harvestDelay;
+
+        unchecked {
+            if (block.timestamp >= previousHarvest + harvestInterval)
+                return 0;
+            uint256 maximumLockedProfit = maxLockedProfit;
+            return maximumLockedProfit - (maximumLockedProfit * (block.timestamp - previousHarvest)) / harvestInterval;
+        }
+    }
+
+    function totalFloat() public view returns (uint256) {
+        return UNDERLYING.balanceOf(address(this));
+    }
+
+    event Initialized(address indexed user);
+    bool public isInitialized;
+
+    function initialize() external requiresAuth {
+        require(!isInitialized, "ALREADY_INITIALIZED");
+
+        isInitialized = true;
+        totalSupply = 0;
+        emit Initialized(msg.sender);
+    }
+
+    function destroy() external requiresAuth {
+        selfdestruct(payable(msg.sender));
+    }
 }
