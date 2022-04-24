a7d655e85a63a0c045bb4b19f63453a014be2804
André Dal Bosco <andredbsc@gmail.com>
2022-04-24 07:52:01 +0200

    update contract

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,29 +3,149 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import {ERC20} from "./ERC20.sol";
-import {ERC4626} from "./ERC4626.sol";
-import {FixedPointMathLib} from "./FixedPointMathLib.sol";
-import {SafeTransferLib} from "./SafeTransferLib.sol";
+import {ERC20} from "./lib/ERC20.sol";
+import {ERC4626} from "./lib/ERC4626.sol";
+import {FixedPointMathLib} from "./lib/FixedPointMathLib.sol";
+import {SafeTransferLib} from "./lib/SafeTransferLib.sol";
 
 contract Vault is ERC4626 {
     using FixedPointMathLib for uint256;
-    using SafeTransferLib for ERC20;
 
-    ERC20 public immutable UNDERLYING;
+    ERC20 public immutable underlyingToken;
 
-    constructor(ERC20 _UNDERLYING)
-        ERC4626(
-            _UNDERLYING,
-            string(abi.encodePacked("Leibniz ", _UNDERLYING.name(), " Vault")),
-            string(abi.encodePacked("ppn", _UNDERLYING.symbol()))
-        )
-    {
-        UNDERLYING = _UNDERLYING;
+    constructor(ERC20 _underlyingToken) ERC4626(
+        _underlying_token,
+        "Leibniz Vault",
+        "PPN"
+    ) {
+        underlyingToken = _underlyingToken;
         totalSupply = type(uint256).max;
     }
 
     function totalAssets() public pure override returns (uint256 totalUnderlyingYield) {
         return 100;
+    }
+
+    function afterDeposit(uint256, uint256) internal override {}
+
+    function beforeWithdraw(uint256 assets, uint256) internal override {
+        // Retrieve underlying tokens from strategies/float.
+        retrieveUnderlying(assets);
+    }
+
+    /// @dev Retrieves a specific amount of underlying tokens held in strategies and/or float.
+    /// @dev Only withdraws from strategies if needed and maintains the target float percentage if possible.
+    /// @param underlyingAmount The amount of underlying tokens to retrieve.
+    function retrieveUnderlying(uint256 underlyingAmount) internal {
+        // Get the Vault's floating balance.
+        uint256 float = totalFloat();
+
+        // If the amount is greater than the float, withdraw from strategies.
+        if (underlyingAmount > float) {
+            // Compute the amount needed to reach our target float percentage.
+            uint256 floatMissingForTarget = (totalAssets() - underlyingAmount).mulWadDown(targetFloatPercent);
+
+            // Compute the bare minimum amount we need for this withdrawal.
+            uint256 floatMissingForWithdrawal = underlyingAmount - float;
+
+            // Pull enough to cover the withdrawal and reach our target float percentage.
+            pullFromWithdrawalStack(floatMissingForWithdrawal + floatMissingForTarget);
+        }
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        VAULT ACCOUNTING LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns a user's Vault balance in underlying tokens.
+    /// @param user The user to get the underlying balance of.
+    /// @return The user's Vault balance in underlying tokens.
+    function balanceOfUnderlying(address user) external view returns (uint256) {
+        return balanceOf[user].mulDivDown(exchangeRate(), BASE_UNIT);
+    }
+
+    /// @notice Returns the amount of underlying tokens an rvToken can be redeemed for.
+    /// @return The amount of underlying tokens an rvToken can be redeemed for.
+    function exchangeRate() public view returns (uint256) {
+        // Get the total supply of rvTokens.
+        uint256 rvTokenSupply = totalSupply;
+
+        // If there are no rvTokens in circulation, return an exchange rate of 1:1.
+        if (rvTokenSupply == 0) return BASE_UNIT;
+
+        // Calculate the exchange rate by dividing the total holdings by the rvToken supply.
+        return totalAssets().mulDivDown(BASE_UNIT, rvTokenSupply);
+    }
+
+    /// @notice Calculates the total amount of underlying tokens the Vault holds.
+    /// @return totalUnderlyingHeld The total amount of underlying tokens the Vault holds.
+    function totalAssets() public view override returns (uint256 totalUnderlyingHeld) {
+        unchecked {
+            // Cannot underflow as locked profit can't exceed total strategy holdings.
+            totalUnderlyingHeld = totalStrategyHoldings - lockedProfit();
+        }
+
+        // Include our floating balance in the total.
+        totalUnderlyingHeld += totalFloat();
+    }
+
+    /// @notice Calculates the current amount of locked profit.
+    /// @return The current amount of locked profit.
+    function lockedProfit() public view returns (uint256) {
+        // Get the last harvest and harvest delay.
+        uint256 previousHarvest = lastHarvest;
+        uint256 harvestInterval = harvestDelay;
+
+        unchecked {
+            // If the harvest delay has passed, there is no locked profit.
+            // Cannot overflow on human timescales since harvestInterval is capped.
+            if (block.timestamp >= previousHarvest + harvestInterval) return 0;
+
+            // Get the maximum amount we could return.
+            uint256 maximumLockedProfit = maxLockedProfit;
+
+            // Compute how much profit remains locked based on the last harvest and harvest delay.
+            // It's impossible for the previous harvest to be in the future, so this will never underflow.
+            return maximumLockedProfit - (maximumLockedProfit * (block.timestamp - previousHarvest)) / harvestInterval;
+        }
+    }
+
+    /// @notice Returns the amount of underlying tokens that idly sit in the Vault.
+    /// @return The amount of underlying tokens that sit idly in the Vault.
+    function totalFloat() public view returns (uint256) {
+        return UNDERLYING.balanceOf(address(this));
+    }
+
+     /*///////////////////////////////////////////////////////////////
+                    INITIALIZATION AND DESTRUCTION LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when the Vault is initialized.
+    /// @param user The authorized user who triggered the initialization.
+    event Initialized(address indexed user);
+
+    /// @notice Whether the Vault has been initialized yet.
+    /// @dev Can go from false to true, never from true to false.
+    bool public isInitialized;
+
+    /// @notice Initializes the Vault, enabling it to receive deposits.
+    /// @dev All critical parameters must already be set before calling.
+    function initialize() external requiresAuth {
+        // Ensure the Vault has not already been initialized.
+        require(!isInitialized, "ALREADY_INITIALIZED");
+
+        // Mark the Vault as initialized.
+        isInitialized = true;
+
+        // Open for deposits.
+        totalSupply = 0;
+
+        emit Initialized(msg.sender);
+    }
+
+    /// @notice Self destructs a Vault, enabling it to be redeployed.
+    /// @dev Caller will receive any ETH held as float in the Vault.
+    function destroy() external requiresAuth {
+        selfdestruct(payable(msg.sender));
     }
 }
